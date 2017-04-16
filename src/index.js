eb08d43370e0cc2f927048641664dbc3c8ee4812
Aaron Suggs <aaron@ktheory.com>
2017-04-15 21:59:01 -0400

    It's working!

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ const S = require('sanctuary');
 
 var GAME_STATES = {
     TRIVIA: "_TRIVIAMODE", // Asking trivia questions.
-    START: "_STARTMODE", // Entry point, start the game.
-    HELP: "_HELPMODE" // The user is asking for help.
+    START: "_STARTMODE" // Entry point, start the game.
 };
 //var questions = require("./questions");
 const questions = require("./states");
 
-const GAME_LENGTH = questions.length;  // The number of questions per trivia game.
 /**
  * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
  * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
@@ -20,24 +18,19 @@ var languageString = {
     "en": {
         "translation": {
             "GAME_NAME" : "State Capitals Quiz", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "I will ask you a question about state capitals, and you say the answer. " +
-            "To start a new game at any time, say, start game. ",
             "REPEAT_QUESTION_MESSAGE": "To repeat the last question, say, repeat. ",
             "ASK_MESSAGE_START": "Would you like to start playing?",
-            "HELP_REPROMPT": "To give an answer to a question, respond with the name of the state or capital. ",
-            "STOP_MESSAGE": "Would you like to keep playing?",
+            "STOP_MESSAGE": "Okay. Goodbye.",
             "CANCEL_MESSAGE": "Ok, let\'s play again soon.",
             "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye!",
             "TRIVIA_UNHANDLED": "Try saying the name of a state or capital",
-            "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
             "START_UNHANDLED": "Say start to start a new game.",
             "NEW_GAME_MESSAGE": "Hi Adrian. Let\'s practice some state capitals. ",
             "ANSWER_CORRECT_MESSAGE": "Correct. ",
             "ANSWER_WRONG_MESSAGE": "That\'s incorrect. ",
             "CORRECT_ANSWER_MESSAGE": "The correct answer is %s. ",
             "ANSWER_IS_MESSAGE": "That answer is ",
-            "GAME_OVER_MESSAGE": "You got %s out of %s questions correct. Thank you for playing!",
-            "SCORE_IS_MESSAGE": "Your score is %s. "
+            "GAME_OVER_MESSAGE": "You got %s out of %s questions correct. Thank you for playing!"
         }
     },
     "en-US": {
@@ -47,12 +40,13 @@ var languageString = {
     }
 };
 
-exports.handler = function(event, context, callback) {
+exports.handler = (event, context, callback) => {
+    console.log("Event: " + JSON.stringify(event));
     const alexa = Alexa.handler(event, context);
     alexa.appId = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageString;
-    alexa.registerHandlers(newSessionHandlers, startStateHandlers, triviaStateHandlers, helpStateHandlers);
+    alexa.registerHandlers(newSessionHandlers, startStateHandlers, triviaStateHandlers);
     alexa.execute();
 };
 
@@ -65,10 +59,6 @@ var newSessionHandlers = {
         this.handler.state = GAME_STATES.START;
         this.emitWithState("StartGame", true);
     },
-    "AMAZON.HelpIntent": function() {
-        this.handler.state = GAME_STATES.HELP;
-        this.emitWithState("helpTheUser", true);
-    },
     "Unhandled": function () {
         var speechOutput = this.t("START_UNHANDLED");
         this.emit(":ask", speechOutput, speechOutput);
@@ -78,9 +68,8 @@ var newSessionHandlers = {
 const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
     "StartGame": function (newGame) {
         var speechOutput = newGame ? this.t("NEW_GAME_MESSAGE") : "";
-        // Select GAME_LENGTH questions for the game
         //var gameQuestions = questions;
-        const currentQuestionIndex = 0;
+        const currentQuestionIndex = nextQuestionIndex(questions)
         var spokenQuestion = questions[currentQuestionIndex].question;
         var repromptText = spokenQuestion;
 
@@ -90,8 +79,6 @@ const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
             "speechOutput": repromptText,
             "repromptText": repromptText,
             "currentQuestionIndex": currentQuestionIndex,
-            "questions": questions,
-            "score": 0,
             "correctAnswerText": questions[currentQuestionIndex].answer
         });
 
@@ -115,14 +102,8 @@ var triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
     "AMAZON.RepeatIntent": function () {
         this.emit(":ask", this.attributes["speechOutput"], this.attributes["repromptText"]);
     },
-    "AMAZON.HelpIntent": function () {
-        this.handler.state = GAME_STATES.HELP;
-        this.emitWithState("helpTheUser", false);
-    },
     "AMAZON.StopIntent": function () {
-        this.handler.state = GAME_STATES.HELP;
-        var speechOutput = this.t("STOP_MESSAGE");
-        this.emit(":ask", speechOutput, speechOutput);
+        this.emit(":tell", this.t("CANCEL_MESSAGE"));
     },
     "AMAZON.CancelIntent": function () {
         this.emit(":tell", this.t("CANCEL_MESSAGE"));
@@ -136,56 +117,9 @@ var triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
     }
 });
 
-var helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
-    "helpTheUser": function (newGame) {
-        var askMessage = newGame ? this.t("ASK_MESSAGE_START") : this.t("REPEAT_QUESTION_MESSAGE") + this.t("STOP_MESSAGE");
-        var speechOutput = this.t("HELP_MESSAGE", GAME_LENGTH) + askMessage;
-        var repromptText = this.t("HELP_REPROMPT") + askMessage;
-        this.emit(":ask", speechOutput, repromptText);
-    },
-    "AMAZON.StartOverIntent": function () {
-        this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", false);
-    },
-    "AMAZON.RepeatIntent": function () {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    },
-    "AMAZON.HelpIntent": function() {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    },
-    "AMAZON.YesIntent": function() {
-        if (this.attributes["speechOutput"] && this.attributes["repromptText"]) {
-            this.handler.state = GAME_STATES.TRIVIA;
-            this.emitWithState("AMAZON.RepeatIntent");
-        } else {
-            this.handler.state = GAME_STATES.START;
-            this.emitWithState("StartGame", false);
-        }
-    },
-    "AMAZON.NoIntent": function() {
-        var speechOutput = this.t("NO_MESSAGE");
-        this.emit(":tell", speechOutput);
-    },
-    "AMAZON.StopIntent": function () {
-        var speechOutput = this.t("STOP_MESSAGE");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "AMAZON.CancelIntent": function () {
-        this.emit(":tell", this.t("CANCEL_MESSAGE"));
-    },
-    "Unhandled": function () {
-        var speechOutput = this.t("HELP_UNHANDLED");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "SessionEndedRequest": function () {
-        console.log("Session ended in help state: " + this.event.request.reason);
-    }
-});
-
 const handleUserGuess = (ctx, userGaveUp) => {
 
+  console.log("GUESS: " + userAnswer(ctx));
   let speechOutput = ""
   if (S.equals(correctAnswer(ctx), userAnswer(ctx))) {
     speechOutput += "Correct. "
@@ -195,9 +129,9 @@ const handleUserGuess = (ctx, userGaveUp) => {
   }
 
 
-  const nextQuestionIndex = currentQuestionIndex + 1
-  //const nextQuestionIndex = Math.floor(Math.random() * questions.length)
-  const nextQuestion = questions[nextQuestionIndex].question
+  //const nextQuestionIndex = currentQuestionIndex + 1
+  const idx = nextQuestionIndex(questions)
+  const nextQuestion = questions[idx].question
 
   speechOutput += nextQuestion
 
@@ -205,10 +139,10 @@ const handleUserGuess = (ctx, userGaveUp) => {
   Object.assign(ctx.attributes, {
     "speechOutput": speechOutput,
     "repromptText": nextQuestion,
-    "currentQuestionIndex": nextQuestionIndex
+    "currentQuestionIndex": idx
   });
 
-  ctx.emit(":askWithCard", speechOutput, nextQuestion, ctx.t("GAME_NAME"), nextQuestion);
+  ctx.emit(":ask", speechOutput, nextQuestion);
 }
 
 const userAnswer = (ctx) =>
@@ -218,12 +152,16 @@ const userAnswer = (ctx) =>
   )(ctx)
 
 const correctAnswer = (ctx) =>
-  S.get(
-    S.is(String), "answer"
-  )(S.at(currentQuestionIndex(ctx), questions))
+  S.chain(
+    S.get( S.is(String), "answer"),
+    S.chain(S.at(S.__, questions), currentQuestionIndex(ctx))
+  )
 
 const currentQuestionIndex = (ctx) =>
   S.gets(
     S.is(Number),
     ['attributes', 'currentQuestionIndex']
   )(ctx)
+
+const nextQuestionIndex = (questions) =>
+  Math.floor(Math.random() * questions.length)
